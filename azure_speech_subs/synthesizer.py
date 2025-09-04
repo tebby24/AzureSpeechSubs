892e8672f19be31403183d4742af6bb651c87783
Teddy Gonyea <enterted@gmail.com>
2025-09-03 22:08:10 -0400

    Add import statements and define constructor for SpeechSynthesizer class

--- azure_speech_subs/synthesizer.py
+++ azure_speech_subs/synthesizer.py
@@ -1,5 +1,6 @@
-
-
+import os
+import azure.cognitiveservices.speech as speechsdk
 
 class SpeechSynthesizer:
-    def __init__(azure_speech_key, azure_speech_region)
+    def __init__(azure_speech_key, azure_speech_region):
+        pass
