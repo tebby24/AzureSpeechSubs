2e9dfa062a78ab0b5ceda7e51eb4fa9b7ca29653
Teddy Gonyea <enterted@gmail.com>
2025-09-05 04:31:40 -0400

    Refactor speech synthesis process to generate speech with subtitles directly

--- test.py
+++ test.py
@@ -12,17 +12,8 @@ if __name__ == "__main__":
     with open("test/in/text.txt", "r") as f:
         text = f.read()
     voice = "zh-CN-YunjianNeural"
+    split_characters = ["？", "。", "：", "，", "、", "”"]
 
     synthesizer = SpeechSynthesizer(os.getenv("AZURE_SPEECH_KEY"), os.getenv("AZURE_SPEECH_REGION"))
 
-    # synthesizer.synthesize_speech(text, voice, "test/out")
-
-    with open("test/out/0001.word.json", "r") as f:
-        word_boundaries = json.load(f)
-
-    split_characters = ["？", "。", "：", "，", "、", "”"]
-
-    groups = synthesizer.build_groups(word_boundaries, split_characters)
-    [print(group) for group in groups]
-
-    synthesizer.save_subs(groups, "test/out/srt.srt")
+    synthesizer.generate_speech_with_subtitles(text, voice, "test/out", split_characters)
