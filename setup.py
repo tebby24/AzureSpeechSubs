02df8d9ffa38669047b44e095909cd38a50067d9
Teddy Gonyea <enterted@gmail.com>
2025-09-05 04:36:52 -0400

    Add setup.py for package configuration and dependencies

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="azure-speech-subs",
+    packages=find_packages(), 
+    install_requires=[
+        "srt",
+        "requests",
+    ],
+    author="Teddy Gonyea",
+    description="A tool to generate speech with synchronized subtitles using Azure Cognitive Services.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tebby24/AzureSpeechSubs",
+)
